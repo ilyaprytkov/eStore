e66c521e180ae8c9d4373d12f1d6e10ee6770357
Tambyk <ilyaprytkov.1509@gmail.com>
2026-08-24 20:01:17 +0200

    Added styles to Sidebar and created user component

---
+++ admin/src/app/(withLayout)/users/page.jsx
@@ -0,0 +1,7 @@
+export default function Users() {
+    return (
+        <div>
+            <h1>This is the users page</h1>
+        </div>
+    )
+}
--- admin/src/components/Sidebar/index.jsx
+++ admin/src/components/Sidebar/index.jsx
@@ -1,30 +1,34 @@
 import Link from "next/link";
 import Image from "next/image";
+import { HomeIcon, ShoppingBagIcon, SwatchIcon, UserIcon } from "../icons";
 
 export default function Sidebar() {
     return (
-       <div>
-            <div>
-                <h1>eStore</h1>
+       <div className="sidebar-main">
+            <div className="ml-10 pt-5">
+                <h1 className="text-3xl font-bold">eStore</h1>
             </div>
-            <ul>
+            <ul className="mx-auto text-lg flex flex-col mt-15">
                 {
-                    [{name: "Dashbboard", path: "/"}, 
-                    {name: "Users", path: "/users"}, 
-                    {name: "Product Types", path: "/product-types"}, 
-                    {name: "Products", path: "/products"}].map((item, key) => {
+                    [{name: "Dashbboard", path: "/", icon: <HomeIcon/>}, 
+                    {name: "Users", path: "/users", icon: <UserIcon/>}, 
+                    {name: "Product Type", path: "/product-types", icon: <SwatchIcon/>}, 
+                    {name: "Products", path: "/products", icon: <ShoppingBagIcon/>}].map((item, key) => {
                         return (
                             <li key={key}>
                                 <Link href={item.path}>
-                                    <span>{item.name}</span>
+                                    <div className="sidebar-list-item">
+                                        <span className="mx-2">{item.icon}</span>
+                                        {item.name}
+                                    </div>
                                 </Link>
                             </li>
                         )
                     })
                 }
             </ul>
-            <div>
-                <div>
+            <div className="sidebar-usercard">
+                <div className="flex flex-row m-5 mb-8">
                     <Image
                         height={50}
                         width={50}
@@ -32,7 +36,7 @@ export default function Sidebar() {
                         alt="User Avatar"
                         radius="sm"
                     />
-                    <div>John Doe</div>
+                    <div className="m-auto text-lg">John Doe</div>
                 </div>
             </div>
        </div>
