bf66ff7a60e8c3a2539a70050f891e5c000b20d8
Tambyk <ilyaprytkov.1509@gmail.com>
2026-08-25 22:39:37 +0200

    refactoring code structure

--- admin/src/app/(withLayout)/users/add/page.jsx
+++ admin/src/app/(withLayout)/users/add/page.jsx
@@ -1,37 +1,12 @@
+import AddUser from "@/shared/screens/users/add";
 import {Button}  from "@/shared/ui/button";
 import { Input } from "@/shared/ui/input";
 import Label from "@/shared/ui/label";
 
 export default function() {
-    return(
-        <div>
-            <h1 className="text-3xl font-semibold p-2"> Add User </h1>
-
-            <form className="grid gap-x-6 gap-y-10 mt-10 grid-cols-2 px-2">
-                <div className="grid gap-2">
-                    <Label required={true}>Username</Label>
-                    <Input placeholder="Enter Username"/>
-                </div>
-                <div className="grid gap-2">
-                    <Label required={true}>User Type</Label>
-                    <select className="custom-input appearance-none bg-white cursor-pointer">
-                        <option value="">Select User Type</option>
-                        <option value="Super Admin">Super Admin</option>
-                        <option value="Admin">Admin</option>
-                        <option value="Manager">Manager</option>
-                    </select>
-                </div>
-                <div className="grid gap-2">
-                    <Label required={true}>Password</Label>
-                    <Input placeholder="Example@123"/>
-                </div>
-                <div className="grid gap-2">
-                    <Label required={true}>Confirm Password</Label>
-                    <Input placeholder="Re-enter Password"/>
-                </div>
-
-                <Button className="w-52 col-span-2 mt-2">Submit</Button>
-            </form>
-        </div>
+    return (
+        <>
+            <AddUser/>
+        </>
     )
 }
