5b7d328bf382f7f67589f98cc058817c925243bf
Tambyk <ilyaprytkov.1509@gmail.com>
2026-09-02 12:50:58 +0200

    Adding product types and product models into the schema

---
+++ admin/src/app/(withLayout)/product-type/add/page.jsx
@@ -0,0 +1,9 @@
+import AddProdcutTypes from "@/components/Screens/product-type/add"
+
+const AddProductTypePage = ({searchParams}) => {
+    return (
+        <AddProdcutTypes searchParams={searchParams}/>
+    );
+}
+
+export default AddProductTypePage;
---
+++ admin/src/components/Screens/product-type/add/index.jsx
@@ -0,0 +1,38 @@
+import { View } from "@/features/view";
+import { Button } from "@/shared/ui/button";
+import { Input } from "@/shared/ui/input";
+import Label from "@/shared/ui/label";
+import { use } from "react";
+
+const AddProdcutTypes = ({searchParams}) => {
+    
+    const params = use(searchParams);
+    const errorMessage = params?.errorMessage;
+
+    return (
+          <div>
+            <h1 className="text-3xl font-semibold p-2"> Add Product Type </h1>
+
+            <form 
+                className="grid gap-x-6 gap-y-10 mt-10 grid-cols-2 px-2"
+            >
+                {
+                    <View.Condition if={errorMessage}>
+                        <div className="col-span-2 border-red-500 rounded-xl px-5 py-3 bg-red-50 w-fit">
+                            <span className="text-red-500 col-span-2 text-md my-0 font-500">{errorMessage }</span>
+                        </div>
+                    </View.Condition>
+                }
+                <div className="grid gap-2">
+                    <Label required={true}>Product Type</Label>
+                    <Input placeholder="Enter Product Type" name="name"/>
+                </div>
+                <div className="grid gap-2">
+                </div>
+                <Button className="w-52 col-span-2 mt-2">Submit</Button>
+            </form>
+        </div>
+    )
+}
+
+export default AddProdcutTypes;
